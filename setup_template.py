3fb7438dbfd697debc000a5a8fafbd58992588ea
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2023-08-21 15:45:05 -0400

    Have setup template add a license
    Fix license replacement
    
    Add GPL3
    
    Add a checkmark to the README
    
    Add file encodings
    
    Fix unlicensed print
    
    Fix unlicensed print
    
    Fix MIT detection
    
    Fix MIT license detection
    
    Add extra whitespace

--- setup_template.py
+++ setup_template.py
@@ -1,22 +1,115 @@
 #!/usr/bin python3
-
+import dataclasses
 import os
 import pathlib as pl
+from datetime import datetime
+from typing import Optional
+
+DIR_REPO = pl.Path(__file__).parent
+
+
+@dataclasses.dataclass
+class LicenseInfo:
+    """A class representing information about a software license.
+
+    Attributes:
+        name: The name of the license.
+        year : The year the license was issued.
+        license_holder: The name of the entity holding the license.
+    """
+
+    path: pl.Path
+    year: Optional[int] = None
+    license_holder: str = ""
+
+    @property
+    def name(self) -> str:
+        """Returns the name of the license."""
+        return self.path.name.split("_", 1)[1]
+
+
+def get_licenses() -> list[pl.Path]:
+    """Returns a list of paths to license files in the 'licenses' directory.
+
+    Returns:
+         A list of paths to license files.
+
+    """
+    license_dir = pl.Path(__file__).parent / "licenses"
+    return list(license_dir.glob("LICENSE_*"))
+
+
+def request_license() -> Optional[LicenseInfo]:
+    """Asks the user to select a license.
+
+    Returns:
+        The path to the selected license.
+
+    """
+    license_paths = get_licenses()
+    license_options = [p.name.split("_", 1)[1] for p in license_paths]
+    print("Available licenses:")
+    print("\t0. Unlicensed")
+    for i, option in enumerate(license_options):
+        print(f"\t{i + 1}. {option}")
+    while True:
+        try:
+            choice = int(input("Enter the number of the license you want to use: "))
+            if choice == 0:
+                return None
+            if 0 < choice <= len(license_paths):
+                repo_license = license_paths[choice - 1]
+                break
+            raise ValueError("Invalid choice. Please try again.")
+        except ValueError:
+            print("Invalid choice. Please try again.")
+
+    if repo_license.name == "LICENSE_MIT":
+        license_holder = input("Who is the holder of the license? ")
+        current_year = datetime.now().year
+        return LicenseInfo(repo_license, current_year, license_holder)
+    return LicenseInfo(repo_license)
+
+
+def replace_license(repo_license: Optional[LicenseInfo]) -> None:
+    """Replaces the license file in the repository with the specified license.
+
+    Args:
+        repo_license: The license to replace the current license with. If None, the current
+            license file will be deleted.
+
+    """
+    license_file = DIR_REPO / "LICENSE"
+    if not repo_license:
+        license_file.unlink()
+    else:
+        repo_license.path.replace(license_file)
+
+    for local_license in get_licenses():
+        local_license.unlink()
+    pl.Path(DIR_REPO / "licenses").rmdir()
+
+    if not repo_license or repo_license.name != "MIT":
+        return
+
+    with open(license_file, "r", encoding="utf-8") as file_buffer:
+        content = file_buffer.read()
+    content = content.replace("[year]", str(repo_license.year))
+    content = content.replace("[fullname]", repo_license.license_holder)
+
+    with open(license_file, "w", encoding="utf-8") as file_buffer:
+        file_buffer.write(content)
 
 
 def main():
-    # Ensure working dir == repo root
-    file_self = pl.Path(__file__)
-    dir_repo = file_self.parent
-
     # Collect some data
     git_uncommitted_changes = (
-        os.popen(f"git -C {dir_repo} status -s").read().strip() != ""
+        os.popen(f"git -C {DIR_REPO} status -s").read().strip() != ""
     )
-    git_username = os.popen(f"git -C {dir_repo} config user.name").read().strip()
-    git_email = os.popen(f"git -C {dir_repo} config user.email").read().strip()
+    git_username = os.popen(f"git -C {DIR_REPO} config user.name").read().strip()
+    git_email = os.popen(f"git -C {DIR_REPO} config user.email").read().strip()
     git_repo_name = (
-        os.popen(f"git -C {dir_repo} remote get-url origin")
+        os.popen(f"git -C {DIR_REPO} remote get-url origin")
         .read()
         .split("/")[-1]
         .split(".")[0]
@@ -36,6 +129,7 @@ def main():
         input("Enter a short description of the project: ")
         or "A beautiful description."
     )
+    repo_license = request_license()
 
     # Print the data
     print(
@@ -43,23 +137,27 @@ def main():
         f"\tRepository name: '{repo_name}'\n"
         f"\tModule name: '{module_name}'\n"
         f"\tAuthor: '{username} <{email}'>\n"
-        f"\tDescription: '{description}'"
+        f"\tDescription: '{description}'\n"
+        f"\tLicense: '{repo_license.name if repo_license else 'Unlicensed'}'"
     )
     input("Press enter to continue...")
 
     # Replace the template values
-    for file in pl.Path(dir_repo).glob("**/*"):
+    for file in pl.Path(DIR_REPO).glob("**/*"):
         if (
             file.is_file()
-            and not file.name == file_self.name
+            and not file.name == __file__
             and file.suffix in [".py", ".md", ".yml", ".yaml", ".toml", ".txt"]
         ):
-            with open(file, "r") as f:
+            with open(file, "r", encoding="utf-8") as f:
                 content = f.read()
 
             content_before = content
             content = content.replace(
                 "- [ ] Run `setup_template.py`", "- [x] Run `setup_template.py`"
+            )
+            content = content.replace(
+                "- [ ] Update the `LICENSE`", "- [x] Update the `LICENSE`"
             )
             content = content.replace("template-python-repository", repo_name)
             content = content.replace("APP_NAME", module_name)
@@ -70,16 +168,18 @@ def main():
             content = content.replace("Reinder Vos de Wael", username)
 
             if not content == content_before:
-                print(f"Updating {file.relative_to(dir_repo)}")
-                with open(file, "w") as f:
+                print(f"Updating {file.relative_to(DIR_REPO)}")
+                with open(file, "w", encoding="utf-8") as f:
                     f.write(content)
 
-    dir_module = dir_repo / "src" / "APP_NAME"
+    replace_license(repo_license)
+
+    dir_module = DIR_REPO / "src" / "APP_NAME"
     if dir_module.exists():
         dir_module.rename(dir_module.parent / module_name)
 
     # Remove this file
-    print(f"Removing {file_self.name}")
+    print(f"Removing {__file__}")
     pl.Path(__file__).unlink(missing_ok=True)
 
 
