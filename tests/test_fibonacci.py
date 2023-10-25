d3bee883237ca2325624ea3737397e9fca7da6e1
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2023-10-25 18:18:54 -0400

    Add Ruff linter/formatter (#45)
    * Add Ruff linter/formatter
    
    * Add isort rule checks

--- tests/test_fibonacci.py
+++ tests/test_fibonacci.py
@@ -1,3 +1,4 @@
+"""Tests for the fibonacci() function."""
 import pytest
 
 from APP_NAME import algorithms
@@ -5,7 +6,6 @@ from APP_NAME import algorithms
 
 def test_fibonacci_success_0() -> None:
     """Test that fibonacci() returns the correct value for valid input."""
-
     output = algorithms.fibonacci(0)
 
     assert output == 0
@@ -13,21 +13,20 @@ def test_fibonacci_success_0() -> None:
 
 def test_fibonacci_success_18() -> None:
     """Test that fibonacci() returns the correct value for valid input."""
+    expected = 4181
 
-    output = algorithms.fibonacci(19)
+    actual = algorithms.fibonacci(19)
 
-    assert output == 4181
+    assert actual == expected
 
 
 def test_fibonacci_negative() -> None:
     """Test that fibonacci() raises an exception for negative input."""
-
     with pytest.raises(ValueError):
         algorithms.fibonacci(-1)
 
 
 def test_fibonacci_non_integer() -> None:
     """Test that fibonacci() raises an exception for non-integer input."""
-
     with pytest.raises(ValueError):
         algorithms.fibonacci(3.14)  # type: ignore[arg-type]
